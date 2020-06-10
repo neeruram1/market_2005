ac0874f8c738f54b74aebbf71240bc951693fdc7
“Neeru <neeram85@gmail.com>
2020-06-10 09:15:40 -0600

    Vendor class and methods complete

---
+++ test/vendor_test.rb
@@ -0,0 +1,46 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/item'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_it_has_attributes
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+  end
+
+  def test_it_starts_with_no_inventory
+    assert_equal ({}), @vendor.inventory
+  end
+
+  def test_it_can_check_stock
+    assert_equal 0, @vendor.check_stock(@item1)
+  end
+
+  def test_it_can_stock_items
+    @vendor.stock(@item1, 30)
+    expected = {@item1 => 30}
+    assert_equal expected, @vendor.inventory
+    assert_equal 30, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item1, 25)
+    assert_equal 55, @vendor.check_stock(@item1)
+
+    @vendor.stock(@item2, 12)
+    expected1 = {
+              @item1 => 55,
+              @item2 => 12
+              }
+    assert_equal expected1, @vendor.inventory
+  end
+end
